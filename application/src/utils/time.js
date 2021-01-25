73403a4c7b2f3c5354863f56657389b12db4d144
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-01-25 06:04:55 -0800

    refactor(application): use string padding instead of toTimeString

--- application/src/utils/time.js
+++ application/src/utils/time.js
@@ -3,4 +3,10 @@
  * 
  * @param {Date} date - The date to format. 
  */
-export const toHHMMSSTimeString = (date) => date.toTimeString().substring(0, 8);
+export const toHHMMSSTimeString = (date) => {
+    const hours = `${date.getHours()}`.padStart(2, '0');
+    const minutes = `${date.getMinutes()}`.padStart(2, '0');
+    const seconds = `${date.getSeconds()}`.padStart(2, '0');
+    
+    return `${hours}:${minutes}:${seconds}`;
+}
